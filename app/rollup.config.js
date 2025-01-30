9f3089e4e0b25c3b566b19b701e6bc6cec2c89ef
Chris Sauve <chrismsauve@gmail.com>
2025-01-30 01:05:20 -0500

    Add asset handling

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -2,6 +2,6 @@ import {quiltApp} from '@quilted/rollup/app';
 import {cloudflareWorkers} from '@quilted/cloudflare/craft';
 
 export default quiltApp({
-  browser: {entry: './browser.tsx'},
+  browser: {entry: './browser.tsx', assets: {baseURL: '/assets/app/'}},
   server: {entry: './server.tsx', runtime: cloudflareWorkers()},
 });
--- dispatch/dispatch.ts
+++ dispatch/dispatch.ts
@@ -1,10 +1,17 @@
-import type {DispatchNamespace} from '@cloudflare/workers-types';
+import type {R2Bucket, DispatchNamespace} from '@cloudflare/workers-types';
 
 export interface Environment {
+  APP_ASSETS: R2Bucket;
   SCOREKEEP_BRANCHES: DispatchNamespace;
 }
 
-function dispatch(request: Request, env: Environment) {
+async function dispatch(request: Request, env: Environment) {
+  const url = new URL(request.url);
+  if (url.pathname.startsWith('/assets/app/')) {
+    const asset = await fetchAssetFromBucket(request, env.APP_ASSETS);
+    return asset;
+  }
+
   const tophat = request.headers.get('Scorekeep-Web-Tophat');
 
   const worker = tophat
@@ -12,6 +19,23 @@ function dispatch(request: Request, env: Environment) {
     : env.SCOREKEEP_BRANCHES.get(`scorekeep-web:main`);
 
   return worker.fetch(request as any);
+}
+
+async function fetchAssetFromBucket(request: Request, bucket: R2Bucket) {
+  const url = new URL(request.url);
+  const object = await bucket.get(url.pathname.slice(1));
+
+  if (!object || !object.body) {
+    return new Response('Not Found', {status: 404});
+  }
+
+  const headers = new Headers();
+  object.writeHttpMetadata(headers as any);
+  headers.set('etag', object.httpEtag);
+
+  return new Response(object.body as any, {
+    headers,
+  });
 }
 
 export default {fetch: dispatch};
