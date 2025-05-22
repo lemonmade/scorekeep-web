6870bc8c9f98814132ae195d09c6df2d8a59c837
Chris Sauve <chrismsauve@gmail.com>
2025-05-21 22:40:04 -0400

    Try to fix deploy request SHA (#7)
    * Try to fix deploy request SHA
    
    * Test preview
    
    * Add branch name-based deployment
    
    * Undo preview test
    
    * Clean up preview logs
    
    * More log fixes

--- app/scripts/deploy-preview.ts
+++ app/scripts/deploy-preview.ts
@@ -1,52 +1,83 @@
+import {parseArgs} from 'node:util';
 import {Cloudflare} from 'cloudflare';
 import spawn from 'nano-spawn';
 
 const cloudflare = new Cloudflare();
 
-const {output: gitHash} = await spawn('git', ['rev-parse', 'HEAD']);
+const {commit, branch} = await (async () => {
+  const {values} = parseArgs({
+    options: {
+      commit: {type: 'string'},
+      branch: {type: 'string'},
+    },
+  });
+
+  const commit =
+    values.commit ?? (await spawn('git', ['rev-parse', 'HEAD'])).output;
+  const branch =
+    values.branch ??
+    (await spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD'])).output;
+
+  return {commit, branch};
+})();
 
 const dispatchNamespace = 'scorekeep-web-versions';
-const dispatchName = `scorekeep-web.preview.${gitHash}`;
 
-const deployCommand = spawn('pnpm', [
-  `exec`,
-  `wrangler`,
-  `deploy`,
-  `--dispatch-namespace`,
-  dispatchNamespace,
-  `--name`,
-  dispatchName,
-]);
+// I could do these in parallel, but I don’t want to interleave the output of
+// the two deploy commands.
+await deployWithName(`scorekeep-web.preview.${commit}`);
+await deployWithName(`scorekeep-web.preview.${branch}`);
 
-console.log('wrangler deploy:');
-for await (const line of deployCommand) {
-  console.log(line);
-}
+async function deployWithName(dispatchName: string) {
+  const deployCommand = spawn('pnpm', [
+    `exec`,
+    `wrangler`,
+    `deploy`,
+    `--dispatch-namespace`,
+    dispatchNamespace,
+    `--name`,
+    dispatchName,
+  ]);
 
-// Note: this approach works fine for the preview environment, but it wouldn’t
-// work for the production environment, since there would be some time before
-// all the necessary secrets are available.
+  if (process.env.GITHUB_ACTIONS) {
+    console.log(`::group::Deploying preview ${dispatchName}...`);
+  } else {
+    console.log();
+    console.log(`Deploying preview ${dispatchName}...`);
+  }
 
-console.log();
-console.log(`Adding secrets...`);
-const secrets = new Map([
-  ['CLOUDFLARE_API_TOKEN', process.env.CLOUDFLARE_API_TOKEN ?? ''],
-]);
+  for await (const line of deployCommand) {
+    console.log(line);
+  }
 
-for (const [name, value] of secrets) {
-  const result =
-    await cloudflare.workersForPlatforms.dispatch.namespaces.scripts.secrets.update(
-      dispatchNamespace,
-      dispatchName,
-      {
-        account_id: process.env.CLOUDFLARE_ACCOUNT_ID!,
-        name,
-        text: value,
-        type: 'secret_text',
-      },
-    );
+  // Note: this approach works fine for the preview environment, but it wouldn’t
+  // work for the production environment, since there would be some time before
+  // all the necessary secrets are available.
 
   console.log();
-  console.log(`Secret updated: ${name}`);
-  console.log(result);
+  console.log(`Updating secrets...`);
+  const secrets = new Map([
+    ['CLOUDFLARE_API_TOKEN', process.env.CLOUDFLARE_API_TOKEN ?? ''],
+  ]);
+
+  for (const [name, value] of secrets) {
+    const result =
+      await cloudflare.workersForPlatforms.dispatch.namespaces.scripts.secrets.update(
+        dispatchNamespace,
+        dispatchName,
+        {
+          account_id: process.env.CLOUDFLARE_ACCOUNT_ID!,
+          name,
+          text: value,
+          type: 'secret_text',
+        },
+      );
+
+    console.log();
+    console.log(result);
+  }
+
+  if (process.env.GITHUB_ACTIONS) {
+    console.log(`::endgroup::`);
+  }
 }
