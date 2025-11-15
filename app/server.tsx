2d47fc8ecbb3883f76dcdc6453a7562d88cdb576
Chris Sauve <chrismsauve@gmail.com>
2025-11-15 00:43:02 -0500

    Combine dispatch and app worker

--- app/server.tsx
+++ app/server.tsx
@@ -8,26 +8,68 @@ import {
   PermissionsPolicyHeader,
   StrictTransportSecurityHeader,
 } from '@quilted/quilt/server';
-import type {WorkerVersionMetadata} from '@cloudflare/workers-types';
-
+import type {
+  R2Bucket,
+  DispatchNamespace,
+  WorkerVersionMetadata,
+} from '@cloudflare/workers-types';
+import type {ShareMatchService} from '@scorekeep/worker-share-match';
 import {BrowserAssets} from 'quilt:module/assets';
 
 import type {AppContext} from '~/shared/context.ts';
 
 import {App} from './App.tsx';
 
-interface Env {
-  CLOUDFLARE_VERSION_METADATA: WorkerVersionMetadata;
+export interface Environment {
+  APP_ASSETS: R2Bucket;
+  SHARE_MATCH: Pick<ShareMatchService, 'share' | 'ogImage'>;
+  SCOREKEEP_VERSIONS: DispatchNamespace;
+  CLOUDFLARE_VERSION_METADATA?: WorkerVersionMetadata;
   CLOUDFLARE_API_TOKEN: string;
 }
 
+const PREVIEW_HEADER = 'ScoreKeep-Internal-Preview';
 const WORKER_VERSION_ID_RESPONSE_HEADER =
   'ScoreKeep-Internal-Worker-Version-ID';
 const WORKER_VERSION_TAG_RESPONSE_HEADER =
   'ScoreKeep-Internal-Worker-Version-Tag';
 
-const app = new Hono<{Bindings: Env}>();
+const app = new Hono<{Bindings: Environment}>();
 const assets = new BrowserAssets();
+
+// Preview deployment
+app.use('*', async (c, next) => {
+  if (c.env.CLOUDFLARE_VERSION_METADATA) return await next();
+  
+  const preview = c.req.header(PREVIEW_HEADER);
+  if (!preview) return await next();
+
+  const worker = c.env.SCOREKEEP_VERSIONS.get(`scorekeep-web.preview.${preview}`);
+  const response = await worker.fetch(c.req.raw as any);
+  return new ClonedResponse(response as any, {
+    headers: {
+      [PREVIEW_HEADER]: preview,
+    },
+  });
+});
+
+app.get('/assets/app/*', async (c) => {
+  console.log(`Serving app asset...`);
+  const asset = await fetchAssetFromBucket(c.req.raw, c.env.APP_ASSETS);
+  return asset;
+});
+
+app.post('/.internal/share-match', async (c) => {
+  console.log(`Sharing match...`);
+  const response = await c.env.SHARE_MATCH.share(c.req.raw);
+  return response;
+});
+
+app.get('/.internal/share-match/:id/og-image', async (c) => {
+  console.log(`Serving OG image (id: ${c.req.param('id')})...`);
+  const response = await c.env.SHARE_MATCH.ogImage(c.req.raw);
+  return response;
+});
 
 // For all GET requests, render our Preact application.
 app.get('*', async (c) => {
@@ -132,3 +174,57 @@ app.get('*', async (c) => {
 });
 
 export default app;
+
+class ClonedResponse extends Response {
+  constructor(response: Response, init?: ResponseInit) {
+    const headers = init?.headers
+      ? new ClonedHeaders(response.headers, init.headers)
+      : response.headers;
+
+    super(response.body as any, {
+      ...init,
+      status: response.status,
+      statusText: response.statusText,
+      headers,
+    });
+  }
+}
+
+class ClonedHeaders extends Headers {
+  constructor(headers: Headers, init?: HeadersInit) {
+    super(headers);
+
+    if (init) {
+      if (init instanceof Headers) {
+        for (const [key, value] of init.entries()) {
+          this.append(key, value);
+        }
+      } else if (Array.isArray(init)) {
+        for (const [key, value] of init) {
+          this.append(key, value);
+        }
+      } else {
+        for (const [key, value] of Object.entries(init)) {
+          this.append(key, value);
+        }
+      }
+    }
+  }
+}
+
+async function fetchAssetFromBucket(request: Request, bucket: R2Bucket) {
+  const url = new URL(request.url);
+  const object = await bucket.get(url.pathname.slice(1));
+
+  if (!object || !object.body) {
+    return new Response('Not Found', {status: 404});
+  }
+
+  const headers = new Headers();
+  object.writeHttpMetadata(headers as any);
+  headers.set('etag', object.httpEtag);
+
+  return new Response(object.body as any, {
+    headers,
+  });
+}
--- dispatch/rollup.config.js
+++
@@ -1,7 +0,0 @@
-import {quiltServer} from '@quilted/rollup/server';
-import {cloudflareWorkers} from '@quilted/cloudflare/craft';
-
-export default quiltServer({
-  entry: './dispatch.ts',
-  runtime: cloudflareWorkers(),
-});
